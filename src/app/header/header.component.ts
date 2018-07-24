c45ace8723c4d3eca97d2fafaf788fcac4938779
UILeaders Prita Hasjim <phasjim@gmail.com>
2018-07-24 18:26:33 +0300

    Fixed header bug where the menu doesn't close on resize

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class HeaderComponent implements OnInit {
   isMenuDisplayed: boolean = false;
@@ -16,5 +19,12 @@ export class HeaderComponent implements OnInit {
   onDisplayMenuClick(isDisplayed: boolean) {
     this.isMenuDisplayed = isDisplayed;
   }
-  
+
+  /* Automatically closes the menu if the window width >= 600px */
+  onResize(event){
+    if(event.target.innerWidth >= 600) {
+      this.isMenuDisplayed = false;
+    }
+  }
+
 }
